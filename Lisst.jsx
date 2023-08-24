0c4ca6d34fdac6597e88a504770a3921e64d9032
Mahdiyeh <www.mahdiyeh.mazani@gmail.com>
2023-08-25 00:45:27 +0330

    Add files via upload

---
+++ Lisst.jsx
@@ -0,0 +1,11 @@
+import './lisst.module.css';
+const Lisst =(props)=>{
+    return(
+        <div style={{backgroundColor:props.list.isCompleted ?"green" : "red"}} key={`div_${props.index}`}>
+          <h1  key={`h1_${props.index}`}>{props.list.listname}</h1>
+          <button  key={`btn_${props.index}`} onClick={()=>props.deletelist(props.list.id)}>X</button>
+        <button onClick={()=>{props.completelist(props.list.id)}}>Completed</button>
+        </div>
+    )
+}
+export default Lisst
